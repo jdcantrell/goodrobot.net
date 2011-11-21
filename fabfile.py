c19411cc4dd7f8d0b203011476192180d7d1722d
JD Cantrell <jdcantrell@gmail.com>
2011-11-20 22:40:50 -0800

    Update deploy script to generate the stream page after deploy

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,10 @@
-from __future__ import with statement
-from fabric.api import local, settings, abort
+from __future__ import with_statement
+from fabric.api import local, run, cd
+
+
+#TODO: Re-organize site so that the main blog is not in the base directory
+#this will prevent the stream pages (and anything else that is evntually added
+#from getting destroyed on deploy
 
 def build():
   local("compass compile ./")
@@ -7,8 +12,16 @@ def build():
 
 def deploy():
   code_dir = "~/goodrobot.net"
+  stream_dir = "~/ankh"
+  
+  #actually generate the site
   with cd(code_dir):
     run("git pull")
     run("compass compile ./")
     run("jekyll")
 
+  #re-gen the stream page on deploy
+  with cd(stream_dir):
+    run("git pull")
+    run("mkdir %s/_site/stream" % code_dir)
+    run("python ankh.py -t goodrobot.template.html -o %s/_site/stream/index.html -v" % code_dir)
