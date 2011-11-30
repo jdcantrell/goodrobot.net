bc4118182bfb042b7f1e64e70d4c12c75e6975ee
JD Cantrell <jdcantrell@gmail.com>
2011-11-29 23:13:24 -0800

    Add host to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 from __future__ import with_statement
 from fabric.api import local, run, cd
+from fabric.decorators import hosts
+from fabric.colors import blue, green
 
 
 #TODO: Re-organize site so that the main blog is not in the base directory
@@ -10,18 +12,22 @@ def build():
   local("compass compile ./")
   local("jekyll")
 
+@hosts('jdcantrell@goodrobot.net')
 def deploy():
   code_dir = "~/goodrobot.net"
   stream_dir = "~/ankh"
   
   #actually generate the site
   with cd(code_dir):
+    print(blue("Updating site and building..."))
     run("git pull")
     run("compass compile ./")
     run("jekyll")
 
   #re-gen the stream page on deploy
   with cd(stream_dir):
+    print(blue("Updating ankh and regenerating..."))
     run("git pull")
     run("mkdir %s/_site/stream" % code_dir)
     run("python ankh.py -t goodrobot.template.html -o %s/_site/stream/index.html -v" % code_dir)
+  print(green("Site has been successfully deployed: http://goodrobot.net"))
