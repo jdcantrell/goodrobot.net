a198199e6dbefbd5b0ab43850d761de6263e7fe9
JD Cantrell <jdcantrell@gmail.com>
2014-02-02 01:10:20 -0800

    Cleanup fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,26 +11,30 @@ production = 'jd@goodrobot.net:22'
 dest_path = '/srv/http/goodrobot'
 
 def clean():
-    if os.path.isdir(DEPLOY_PATH):
-        local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
+  if os.path.isdir(DEPLOY_PATH):
+    local('rm -rf {deploy_path}'.format(**env))
+    local('mkdir {deploy_path}'.format(**env))
 
 def build():
-    local('pelican -s pelicanconf.py')
+  local('pelican -s pelicanconf.py')
 
 def rebuild():
-    clean()
-    build()
+  clean()
+  build()
 
 def regenerate():
-    local('pelican -r -s pelicanconf.py')
+  local('pelican -r -s pelicanconf.py')
+
+@hosts(production)
+def push(filename):
+  put(filename, '/srv/http/static/cdn/%s' % filename)
 
 @hosts(production)
 def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
+  local('pelican -s publishconf.py')
+  project.rsync_project(
+    remote_dir=dest_path,
+    exclude=".DS_Store",
+    local_dir=DEPLOY_PATH.rstrip('/') + '/',
+    delete=True
+  )
