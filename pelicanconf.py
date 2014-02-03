cfcab8f219db9910cbfc22b0ca6b21fc74b9dd74
JD Cantrell <jdcantrell@gmail.com>
2014-02-02 17:43:15 -0800

    Dev vs publish confs

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 
 AUTHOR = u'jd'
 SITENAME = u'goodrobot.net'
-SITEURL = 'http://goodrobot.net'
+SITEURL = 'http://goodrobot.dev'
+
+
+# http://gxl.dev/build/gxl.css
+CSS_PATH = 'http://gxl.dev/build/gxl.css'
 
 TIMEZONE = 'Europe/Paris'
 
@@ -14,6 +18,9 @@ DEFAULT_LANG = u'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+
+ARTICLE_URL = '{slug}.html'
+PAGE_URL = '{slug}.html'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://getpelican.com/'),
@@ -29,6 +36,8 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+FEED_ATOM = "atom.xml"
+FEED_RSS = "rss.xml"
 
 THEME = 'theme'
 ARTICLE_DIR = (('posts'))
--- publishconf.py
+++ publishconf.py
@@ -11,12 +11,20 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://goodrobot.net'
+
+CSS_PATH = 'http://static.goodrobot.net/gxl/gxl.min.css'
+
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+ARTICLE_URL = '{slug}'
+PAGE_URL = '{slug}'
+
+
 
 # Following items are often useful when publishing
 
