dd78728b704317f2e27963dd024de3fac7f2498d
JD Cantrell <jd@goodrobot.net>
2017-05-25 00:51:08 -0700

    More ifs updates

--- src/lab/ifs/canvas.js
+++ src/lab/ifs/canvas.js
@@ -10,7 +10,6 @@ class Canvas {
       this.canvas.height = this.canvas.height * window.devicePixelRatio;
     }
 
-    //this.data = new Uint8ClampedArray(this.canvas.width * this.canvas.height * 4).fill(255);
     this.context = this.canvas.getContext('2d');
     this.buffer = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
     this.clearColor = clearColor;
@@ -48,53 +47,3 @@ class Canvas {
   }
 
 }
-
-const mult = ([ a, b, c, d ], [ x, y ]) => [
-  a*x + b*y,
-  c*x + d*y
-];
-
-const add = ([ x, y ], [ tx, ty ]) => [ x + tx, y + ty ];
-
-const ifs = (transforms) => (x, y) => {
-  let prob = Math.floor(Math.random() * (100));
-  return transforms.reduce((xy, { p, transform, translate }) => {
-    if (!xy && p > prob) {
-      return add(mult(transform, [ x, y ]), translate);
-    }
-    prob -= p;
-    return xy;
-  }, false);
-};
-
-const canvas = new Canvas('canvas', [ 255, 255, 238, 255 ]);
-
-function iterate(fn) {
-  let iterations = 500000;
-  const _batch = () => {
-    for (let i = 0; i < 10000; i++) {
-      fn();
-      iterations--;
-    }
-    canvas.flush();
-
-    if (iterations > 0) {
-      window.requestAnimationFrame(() => { _batch() });
-    }
-  }
-  _batch();
-}
-
-
-const render = ({ viewport, ifs }) => {
-  canvas.clear();
-  canvas.setViewport(viewport);
-  let x = 0;
-  let y = 0;
-  iterate(() => {
-    [x, y] = ifs(x, y) || [ 0, 0 ];
-    canvas.setPoint(x, y, [ 24, 105, 57 ]);
-  });
-};
-
-
