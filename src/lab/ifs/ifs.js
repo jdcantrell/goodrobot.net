a4958b61ed02c3f5083c79d46f867e2f46c81c25
JD Cantrell <jd@goodrobot.net>
2017-05-27 13:21:17 -0700

    Theme tweaks, bug fixes to ifs

--- src/lab/ifs/ifs.js
+++ src/lab/ifs/ifs.js
@@ -7,42 +7,67 @@ const add = ([ x, y ], [ tx, ty ]) => [ x + tx, y + ty ];
 
 const ifs = (transforms) => (x, y) => {
   let prob = Math.floor(Math.random() * (100));
-  return transforms.reduce((xy, { p, transform, translate }) => {
+  return transforms.reduce((xy, { p, transform, translate }, idx) => {
     if (!xy && p > prob) {
-      return add(mult(transform, [ x, y ]), translate);
+      return {
+        xy: add(mult(transform, [ x, y ]), translate),
+        idx: idx
+      };
     }
     prob -= p;
     return xy;
   }, false);
 };
 
-const canvas = new Canvas('canvas', [ 255, 255, 238, 255 ]);
+const canvas = new Canvas('canvas', [ 46, 42, 49, 255 ]);
+const emptyFn = () => {}
+let renderFn = emptyFn
+let iterations = 0;
+let clear = false;
 
-function iterate(fn) {
-  let iterations = 500000;
-  const _batch = () => {
+function iterate() {
+  if (clear) {
+    canvas.clear();
+    clear = false;
+  }
+  if (iterations > 0) {
     for (let i = 0; i < 10000; i++) {
-      fn();
+      renderFn();
       iterations--;
     }
     canvas.flush();
-
-    if (iterations > 0) {
-      window.requestAnimationFrame(() => { _batch() });
-    }
   }
-  _batch();
+  window.requestAnimationFrame(iterate);
+}
+
+iterate();
+
+const ctable = [
+  [216, 19, 127],
+  [214, 84, 7],
+  [220, 138, 14],
+  [23, 173, 152],
+  [20, 155, 218],
+  [121, 106, 245],
+  [187, 96, 234],
+  [199, 32, 202],
+
+];
+const colors = (idx) => {
+  return ctable[idx % ctable.length];
 }
 
 const render = ({ viewport, ifs }) => {
-  canvas.clear();
   canvas.setViewport(viewport);
   let x = 0;
   let y = 0;
-  iterate(() => {
-    [x, y] = ifs(x, y) || [ 0, 0 ];
-    canvas.setPoint(x, y, [ 24, 105, 57 ]);
-  });
+  iterations = 500000;
+  clear = true;
+  renderFn = () => {
+    let r = ifs(x, y) || { xy: [0, 0], idx: 0 };
+    [x, y]= r.xy;
+    canvas.setPoint(x, y, colors(r.idx));
+  };
 };
 
 
