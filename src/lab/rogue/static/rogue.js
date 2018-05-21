a4a3ec9db3af267c2303afb7deb134b2fa31fadd
JD Cantrell <jd@goodrobot.net>
2018-05-21 09:54:40 -0700

    Some rogue changes

--- src/lab/rogue/static/rogue.js
+++ src/lab/rogue/static/rogue.js
@@ -1,63 +1,60 @@
-//TODO
-//don't create so many objects so that we can reduce gc
-//add grass animation
-//add player movement
-//add tile interaction
+// TODO
+// don't create so many objects so that we can reduce gc
+// add grass animation
+// add player movement
+// add tile interaction
 const tileMap = {
   '.': {
     type: 'grass',
-    defaults: {
-      render: () => '<span class="grass">.</span>',
-    },
+    render: () => '<span class="grass">.</span>',
+    defaults: {},
   },
   '~': {
     type: 'water',
+    update: (tile, newTile, ticks, pos, neighbors) => {
+      if (tile.shimmer === 0) {
+        if (pos.x === 0) {
+          newTile.shimmer = 5 + Math.floor(Math.random() * Math.floor(10));
+        } else if (
+          (neighbors.w && neighbors.w.state.shimmer > 0) ||
+          (neighbors.n && neighbors.n.state.shimmer > 0) ||
+          (neighbors.s && neighbors.s.state.shimmer > 0)
+        ) {
+          newTile.shimmer = 5;
+        }
+      } else if (tile.shimmer === 1) {
+        // cooldown
+        newTile.shimmer = -5 + -Math.floor(Math.random() * Math.floor(10));
+      } else if (tile.shimmer > 1) {
+        newTile.shimmer--;
+      } else if (tile.shimmer < 0) {
+        newTile.shimmer++;
+      }
+    },
+    render: tile => {
+      if (tile.shimmer > 0) {
+        return '<span class="water shimmering">~</span>';
+      }
+      return '<span class="water">~</span>';
+    },
     defaults: {
       shimmer: 0,
-      update: (tile, newTile, ticks, pos, neighbors) => {
-        if (tile.shimmer === 0) {
-          if (pos.x === 0) {
-            newTile.shimmer = 5 + Math.floor(Math.random() * Math.floor(10));
-          } else if (
-            (neighbors.w && neighbors.w.shimmer > 0) ||
-            (neighbors.n && neighbors.n.shimmer > 0) ||
-            (neighbors.s && neighbors.s.shimmer > 0)
-          ) {
-            newTile.shimmer = 5;
-          }
-        } else if (tile.shimmer === 1) {
-          // cooldown
-          newTile.shimmer = -5 + -Math.floor(Math.random() * Math.floor(10));
-        } else if (tile.shimmer > 1) {
-          newTile.shimmer--;
-        } else if (tile.shimmer < 0) {
-          newTile.shimmer++;
-        }
-      },
-      render: tile => {
-        if (tile.shimmer > 0) {
-          return '<span class="water shimmering">~</span>';
-        }
-        return '<span class="water">~</span>';
-      },
     },
   },
   r: {
     type: 'road',
-    defaults: {
-      render: () => '<span class="road">.</span>',
-    },
+    render: () => '<span class="road">.</span>',
+    defaults: {},
   },
   '#': {
     type: 'wall',
-    defaults: {
-      render: () => '<span class="wall">#</span>',
-    },
+    render: () => '<span class="wall">#</span>',
+    defaults: {},
   },
   '+': {
     type: 'door',
+    render: () => '<span class="door">+</span>',
     defaults: {
-      render: () => '<span class="door">+</span>',
       closed: true,
     },
   },
@@ -69,8 +66,8 @@ const parseLevel = levelText =>
       char =>
         tileMap[char]
           ? {
-              type: tileMap[char].type,
-              ...tileMap[char].defaults,
+              ...tileMap[char],
+              state: { ...tileMap[char].defaults },
             }
           : null
     )
@@ -79,10 +76,12 @@ const parseLevel = levelText =>
 const overworld = parseLevel(document.getElementById('overworld').innerHTML);
 const screen = document.getElementById('screen');
 
-const xy = idx => ({
-  x: idx % 80,
-  y: Math.floor(idx / 80),
-});
+const xyStruct = { x: 0, y: 0 };
+const xy = idx => {
+  xyStruct.x = idx % 80;
+  xyStruct.y = Math.floor(idx / 80);
+  return xyStruct;
+};
 
 const getIdx = (x, y) => {
   if (x >= 0 && x < 80 && y >= 0 && y < 80) {
@@ -91,12 +90,14 @@ const getIdx = (x, y) => {
   return null;
 };
 
-const neighbors = ({ x, y }, data) => ({
-  n: data[getIdx(x, y - 1)],
-  s: data[getIdx(x, y + 1)],
-  w: data[getIdx(x - 1, y)],
-  e: data[getIdx(x + 1, y)],
-});
+const nhStruct = { n: null, s: null, w: null, e: null };
+const neighbors = ({ x, y }, data) => {
+  nhStruct.n = data[getIdx(x, y - 1)];
+  nhStruct.s = data[getIdx(x, y + 1)];
+  nhStruct.w = data[getIdx(x - 1, y)];
+  nhStruct.e = data[getIdx(x + 1, y)];
+  return nhStruct;
+};
 
 const render = (data, ticks) => {
   const newState = [];
@@ -104,17 +105,17 @@ const render = (data, ticks) => {
     // iterate state
     if (tile.update) {
       const pos = xy(idx);
-      const tileState = Object.assign({}, tile);
-      tile.update(tile, tileState, ticks, pos, neighbors(pos, data));
+      const tileState = Object.assign({}, tile.state);
+      tile.update(tile.state, tileState, ticks, pos, neighbors(pos, data));
       if (tileState) {
-        newState[idx] = tileState;
+        newState[idx] = Object.assign({}, tile, { state: tileState });
       }
     } else {
       newState[idx] = tile;
     }
 
     // render
-    const renderedTile = tile.render(newState[idx]);
+    const renderedTile = tile.render(newState[idx].state);
 
     // out to screen
     if (idx && idx % 80 === 0) {
