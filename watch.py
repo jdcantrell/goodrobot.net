b895d68c726137abc8c8fc0111b63755b47c3fde
JD Cantrell <jd@goodrobot.net>
2019-05-03 13:16:07 -0700

    Add initial live reload

---
+++ watch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import glob
+
+from livereload import Server, shell, watcher
+
+
+class RecursiveWatcher(watcher.Watcher):
+    def is_glob_changed(self, path, ignore=None):
+        for f in glob.glob(path, recursive=True):
+            if self.is_file_changed(f, ignore):
+                return True
+        return False
+
+
+server = Server(watcher=RecursiveWatcher())
+server.watch("./**/*.j2", shell("python build.py tpls"))
+server.watch("./**/*.sass", shell("./go.sh css", cwd="./"))
+server.serve(root="./build/")
