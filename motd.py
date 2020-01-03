29c1cc3ccaecc7d6448e123a49f5276a5e54e3f5
JD Cantrell <jd@goodrobot.net>
2020-01-02 22:50:59 -0800

    More motd updates

--- motd.py
+++ motd.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 import json
 import os
 import codecs
@@ -9,8 +9,30 @@ from jinja2 import Environment, FileSystemLoader
 from jinja2.exceptions import TemplateNotFound, TemplateError
 
 
-today = date.today()
-date_string = "{}-{}-{}".format(today.year, today.month, today.day)
+def get_rows():
+    rows = []
+    try:
+        with open("motd.json") as json_file:
+            rows = json.load(json_file)
+    except FileNotFoundError:
+        pass
+
+    return rows
+
+
+def get_date_string():
+    rows = get_rows()
+
+    if len(rows):
+        last_date = datetime.strptime(rows[-1]["date"], "%Y-%m-%d")
+        next_date = last_date + timedelta(days=1)
+    else:
+        next_date = date.today()
+
+    return f"{next_date.year}-{next_date.month:02}-{next_date.day:02}"
+
+
+date_string = get_date_string()
 
 
 def get_date(row):
@@ -21,6 +43,11 @@ def update_entries(row, rows):
     replaced = False
     for idx, r in enumerate(rows):
         if r["date"] == row["date"]:
+            print("\nFound existing row:")
+            print(f" - Date: {r['date']}")
+            print(f" - Title: {r['title']}")
+            print(f" - Image: {r['image']}")
+            click.confirm("Replace existing row?", abort=True)
             rows[idx] = row
             replaced = True
     if not replaced:
@@ -35,7 +62,6 @@ def render(tplfile, data, outfile):
     env = Environment(loader=FileSystemLoader(os.path.abspath("src/")))
     tpl = env.get_template("motd/{}".format(tplfile))
     try:
-        print("render {} {}".format(tplfile, data))
         html = tpl.render(data)
         destfile = codecs.open(outfile, "w", "utf-8")
         destfile.write(html)
@@ -44,15 +70,52 @@ def render(tplfile, data, outfile):
         print("Could not parse template: %s" % err)
 
 
-@click.command()
+def render_list(rows):
+    print("  - Rendering index.html...", end="")
+    render("_list.html.j2", {"rows": rows[::-1]}, "build/motd/index.html")
+    print("done.")
+
+
+def render_day(row, is_today=False):
+    print("  - Rendering {}.html...".format(row["date"]), end="")
+    render("_date.html.j2", row, "build/motd/{}.html".format(row["date"]))
+    print("done.")
+    if is_today:
+        print("  - Rendering today.html...", end="")
+        render("_date.html.j2", row, "build/motd/today.html")
+        print("done.")
+
+
+@click.group()
+def motd():
+    pass
+
+
+@motd.command()
 @click.argument("image", type=click.Path(exists=True))
 @click.option(
-    "--date", prompt="Date", help="The date to add", default=date_string,
+    "--day", prompt="Date", help="The day to add", default=date_string,
 )
 @click.option(
     "--title", prompt="Title", help="The title for the picture",
 )
-def motd(image, date, title):
+def add(image, day, title):
+
+    # validate day
+    if day == "today":
+        next_date = date.today()
+        day = f"{next_date.year}-{next_date.month:02}-{next_date.day:02}"
+    elif day == "tomorrow":
+        next_date = date.today() + timedelta(days=1)
+        day = f"{next_date.year}-{next_date.month:02}-{next_date.day:02}"
+    else:
+        try:
+            user_date = datetime.strptime(day, "%Y-%m-%d")
+            day = f"{user_date.year}-{user_date.month:02}-{user_date.day:02}"
+        except ValueError:
+            print("Cannot parse date {}, please use YYYY-MM-DD".format(day))
+            return
+
     # resize and copy image
     try:
         os.mkdir("src/images/motd")
@@ -63,33 +126,45 @@ def motd(image, date, title):
 
     im.thumbnail((730, 730), Image.ANTIALIAS)
 
-    thumb_filename = "{}.jpg".format(date)
+    thumb_filename = "{}.jpg".format(day)
     thumb_path = os.path.join("src", "images", "motd", thumb_filename)
     rgb_im = im.convert("RGB")
     rgb_im.save(thumb_path, "jpeg", quality=90)
 
     # add new row to file
-    try:
-        with open("motd.json") as json_file:
-            rows = json.load(json_file)
-    except FileNotFoundError:
-        rows = []
-
-    row = {"date": date, "image": thumb_filename, "title": title}
-
-    rows = update_entries(row, rows)
+    row = {"date": day, "image": thumb_filename, "title": title}
+    rows = update_entries(row, get_rows())
     with open("motd.json", "w") as json_file:
-        json.dump(rows, json_file)
+        json.dump(rows, json_file, indent=2)
 
-    render("_date.html.j2", row, "build/motd/{}.html".format(date))
-    render("_date.html.j2", row, "build/motd/today.html")
-    render("_list.html.j2", {"rows": rows[::-1]}, "build/motd/index.html")
+    # link list windows to individual pages
+    # maybe add rsync step?
+    # render_update(rows)
+    # tiles
+    # rss feed
 
 
-#   - date.html
-#   - tiles.html
-#   - today.html
-#   - latest.html
+def is_today_or_earlier(row):
+    last_date = datetime.strptime(row["date"], "%Y-%m-%d")
+    if last_date <= datetime.today():
+        return True
+    return False
+
+
+@motd.command()
+@click.option("--all/--only-latest", default=False)
+def build(all):
+    rows = get_rows()
+    earlier_rows = [row for row in rows if is_today_or_earlier(row)]
+    print("Rendering most recent:")
+    render_list(earlier_rows)
+    render_day(earlier_rows[-1], True)
+
+    if all:
+        print("Rendering previous days:")
+        for row in earlier_rows[:-1]:
+            render_day(row)
+
 
 if __name__ == "__main__":
     motd()
